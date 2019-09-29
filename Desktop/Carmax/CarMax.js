60141737c3fcb46271e6dc3bc80ba16abb8a546c
Araf Rahman <arahman.usa500@gmail.com>
2019-09-29 08:53:21 -0400

    Adding Everything

---
+++ Desktop/Carmax/CarMax.js
@@ -0,0 +1,155 @@
+
+
+function myFunction() {
+  var status = "";
+  var selectObject = document.getElementById("Car");
+
+  var numVal = parseInt(selectObject.value);
+  
+  document.getElementById("rateVal").innerHTML = numVal;
+
+  var elem1 = document.createElement("img");
+  elem1.setAttribute("src", "recars/1C4GY54R1MX533792.png");
+  elem1.setAttribute("height", "350");
+  elem1.setAttribute("width", "350");
+
+  var elem2 = document.createElement("img");
+  elem2.setAttribute("src", "recars/1FCMF53S9Y0A13318.png");
+  elem2.setAttribute("height", "350");
+  elem2.setAttribute("width", "350");
+
+  var elem3 = document.createElement("img");
+  elem3.setAttribute("src", "recars/1FDFE4FLXCDA31544.png");
+  elem3.setAttribute("height", "350");
+  elem3.setAttribute("width", "350");
+
+  var elem4 = document.createElement("img");
+  elem4.setAttribute("src", "recars/1FMPU19565LA65069.png");
+  elem4.setAttribute("height", "350");
+  elem4.setAttribute("width", "350");
+
+  var elem5 = document.createElement("img");
+  elem5.setAttribute("src", "recars/1FTNW20S23EA35695.png");
+  elem5.setAttribute("height", "350");
+  elem5.setAttribute("width", "350");
+
+  var elem6 = document.createElement("img");
+  elem6.setAttribute("src", "recars/1FTSF31Y86ED89642.png");
+  elem6.setAttribute("height", "350");
+  elem6.setAttribute("width", "350");
+
+  var elem7 = document.createElement("img");
+  elem7.setAttribute("src", "recars/1G1YY36U665773082.png");
+  elem7.setAttribute("height", "350");
+  elem7.setAttribute("width", "350");
+
+  var elem8 = document.createElement("img");
+  elem8.setAttribute("src", "recars/1GBGP32V070017982.png");
+  elem8.setAttribute("height", "350");
+  elem8.setAttribute("width", "350");
+
+  var elem9 = document.createElement("img");
+  elem9.setAttribute("src", "recars/1GC2KVCL0C0019531.png");
+  elem9.setAttribute("height", "350");
+  elem9.setAttribute("width", "350");
+
+  var elem10 = document.createElement("img");
+  elem10.setAttribute("src", "recars/1J46258S1VC624980.png");
+  elem10.setAttribute("height", "350");
+  elem10.setAttribute("width", "350");
+
+  var elem11 = document.createElement("img");
+  elem11.setAttribute("src", "recars/1J4GX58S2PC706161.png");
+  elem11.setAttribute("height", "350");
+  elem11.setAttribute("width", "350");
+
+  var elem12 = document.createElement("img");
+  elem12.setAttribute("src", "recars/1JTML65F0J0015235.png");
+  elem12.setAttribute("height", "350");
+  elem12.setAttribute("width", "350");
+
+  var elem13 = document.createElement("img");
+  elem13.setAttribute("src", "recars/1XKDDR9X81R869201.png");
+  elem13.setAttribute("height", "350");
+  elem13.setAttribute("width", "350");
+
+  var elem14 = document.createElement("img");
+  elem14.setAttribute("src", "recars/2HGFG11378H019772.png");
+  elem14.setAttribute("height", "350");
+  elem14.setAttribute("width", "350");
+
+  var elem15 = document.createElement("img");
+  elem15.setAttribute("src", "recars/2XKDDB0X8MM914380.png");
+  elem15.setAttribute("height", "350");
+  elem15.setAttribute("width", "350");
+
+  var elem16 = document.createElement("img");
+  elem16.setAttribute("src", "recars/3G1JC5244XS873514.png");
+  elem16.setAttribute("height", "350");
+  elem16.setAttribute("width", "350");
+
+  var elem17 = document.createElement("img");
+  elem17.setAttribute("src", "recars/3WKDDB0XX9F872748.png");
+  elem17.setAttribute("height", "350");
+  elem17.setAttribute("width", "350");
+
+  var elem18 = document.createElement("img");
+  elem18.setAttribute("src", "recars/4T1BE30K76U151976.png");
+  elem18.setAttribute("height", "350");
+  elem18.setAttribute("width", "350");
+
+  var elem19 = document.createElement("img");
+  elem19.setAttribute("src", "recars/5TFDY5F12BX178431.png");
+  elem19.setAttribute("height", "350");
+  elem19.setAttribute("width", "350");
+
+  var elem20 = document.createElement("img");
+  elem20.setAttribute("src", "recars/5XYKWDA22CG187979.png");
+  elem20.setAttribute("height", "350");
+  elem20.setAttribute("width", "350");
+
+  var elem21 = document.createElement("img");
+  elem21.setAttribute("src", "recars/JM1BJ2234Y0309149.png");
+  elem21.setAttribute("height", "350");
+  elem21.setAttribute("width", "350");
+
+  var elem22 = document.createElement("img");
+  elem22.setAttribute("src", "recars/JM1BL1M58C1614725.png");
+  elem22.setAttribute("height", "350");
+  elem22.setAttribute("width", "350");
+
+  var elem23 = document.createElement("img");
+  elem23.setAttribute("src", "recars/JM1BM1W34E1107719.png");
+  elem23.setAttribute("height", "350");
+  elem23.setAttribute("width", "350");
+
+  var elem24 = document.createElement("img");
+  elem24.setAttribute("src", "recars/JT4RN01P8P7060498.png");
+  elem24.setAttribute("height", "350");
+  elem24.setAttribute("width", "350");
+
+  var elem25 = document.createElement("img");
+  elem25.setAttribute("src", "recars/JYACJ09W46A049833.png");
+  elem25.setAttribute("height", "350");
+  elem25.setAttribute("width", "350");
+
+  var elem26 = document.createElement("img");
+  elem26.setAttribute("src", "recars/KNAFE161X75002771.png");
+  elem26.setAttribute("height", "350");
+  elem26.setAttribute("width", "350");
+
+  var elem27 = document.createElement("img");
+  elem27.setAttribute("src", "recars/KNAGD222155406874.png");
+  elem27.setAttribute("height", "350");
+  elem1.setAttribute("width", "350");
+
+  var elem;
+
+  if (numVal == 1) {
+    elem = elem1;
+  }
+
+
+  document.getElementById("qrCode").appendChild(elem);
+  
+}
